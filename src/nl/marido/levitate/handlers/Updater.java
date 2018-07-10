94b8c033ca8b427f0e33361e13aff535b6653fea
CoderMarido <contact@marido.host>
2018-07-10 18:32:54 +0200

    Updated some stuff and added an automatic update checker.

--- src/nl/marido/levitate/handlers/Updater.java
+++ src/nl/marido/levitate/handlers/Updater.java
@@ -1,9 +1,31 @@
 package nl.marido.levitate.handlers;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+import nl.marido.levitate.Levitate;
+
 public class Updater {
 
 	public static void runChecks() {
-		// This feature has been disabled until the Levitate resource has a valid Resource-ID.
+		boolean checkupdates = Levitate.getInstance().getConfig().getBoolean("check-updates");
+		if (checkupdates) {
+			try {
+				URL checkurl = new URL("https://api.spigotmc.org/legacy/update.php?resource=58576");
+				URLConnection connection = checkurl.openConnection();
+				String latestversion = new BufferedReader(new InputStreamReader(connection.getInputStream())).readLine();
+				String currentversion = Levitate.getInstance().getDescription().getVersion();
+				if (latestversion.equals(currentversion)) {
+					System.out.println("You are running the latest version of Levitate (" + latestversion + ").");
+				} else {
+					System.out.println("You are running an outdated version of Levitate (" + currentversion + ").");
+				}
+			} catch (Exception error) {
+				System.out.println("Failed to connect and check for any newer version of Levitate.");
+			}
+		}
 	}
 
 }
--- src/nl/marido/levitate/listeners/Activate.java
+++ src/nl/marido/levitate/listeners/Activate.java
@@ -26,37 +26,39 @@ public class Activate implements Listener {
 		Action action = event.getAction();
 		if (action == Action.RIGHT_CLICK_AIR || action == Action.RIGHT_CLICK_BLOCK) {
 			Player player = event.getPlayer();
-			Material hand = player.getItemInHand().getType();
-			Material item = Material.BLAZE_ROD;
-			if (hand == item) {
-				UUID uuid = player.getUniqueId();
-				if (entries.contains(uuid)) {
-					player.sendMessage("§eYou have §cdisabled §elevitating successfully.");
-					entries.remove(uuid);
-				} else {
-					player.sendMessage("§eYou have §aenabled §elevitating successfully.");
-					entries.add(uuid);
-					new BukkitRunnable() {
-						public void run() {
-							Material newhand = player.getItemInHand().getType();
-							if (newhand == item || !entries.contains(uuid)) {
-								Location pull = player.getLocation().add(player.getLocation().getDirection().normalize().multiply(10));
-								for (Entity near : pull.getWorld().getNearbyEntities(pull, 7, 7, 7)) {
-									if (near != player) {
-										Location location = near.getLocation();
-										double x = location.getX() - pull.getX();
-										double y = location.getY() - pull.getY();
-										double z = location.getZ() - pull.getZ();
-										Vector velocity = new Vector(x, y, z).normalize().multiply(-0.7);
-										near.setVelocity(velocity);
+			if (player.hasPermission("levitate.activate")) {
+				Material hand = player.getItemInHand().getType();
+				Material item = Material.BLAZE_ROD;
+				if (hand == item) {
+					UUID uuid = player.getUniqueId();
+					if (entries.contains(uuid)) {
+						player.sendMessage("§eYou have §cdisabled §elevitating successfully.");
+						entries.remove(uuid);
+					} else {
+						player.sendMessage("§eYou have §aenabled §elevitating successfully.");
+						entries.add(uuid);
+						new BukkitRunnable() {
+							public void run() {
+								Material newhand = player.getItemInHand().getType();
+								if (newhand == item || !entries.contains(uuid)) {
+									Location pull = player.getLocation().add(player.getLocation().getDirection().normalize().multiply(10));
+									for (Entity near : pull.getWorld().getNearbyEntities(pull, 7, 7, 7)) {
+										if (near != player) {
+											Location location = near.getLocation();
+											double x = location.getX() - pull.getX();
+											double y = location.getY() - pull.getY();
+											double z = location.getZ() - pull.getZ();
+											Vector velocity = new Vector(x, y, z).normalize().multiply(-0.7);
+											near.setVelocity(velocity);
+										}
 									}
+								} else {
+									entries.remove(uuid);
+									cancel();
 								}
-							} else {
-								entries.remove(uuid);
-								cancel();
 							}
-						}
-					}.runTaskTimer(Levitate.getInstance(), 0, 0);
+						}.runTaskTimer(Levitate.getInstance(), 0, 0);
+					}
 				}
 			}
 		}
